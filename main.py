911d6b8c4bd6017f52e20242c0718c3072246270
vadimvshepelev <vadimvshepelev@gmail.com>
2018-07-14 17:11:00 +0300

    start rti test, not debugged yet

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import config as cfg
 import problem as problem_module
 import solver.exact as exc_module
 import eos.ideal as eos_module
-import field as field
+import field as field_module
 import output as output_module
 import app as app_module
 
@@ -33,30 +33,37 @@ eos = eos_module.EOSIdeal(GAMMA=1.4)
 #           problem_module.CProblem(eos, *problem_module.toro_test_5_y)]
 
 # z:
-problem = [problem_module.CProblem(eos, *problem_module.toro_test_1_z),
-           problem_module.CProblem(eos, *problem_module.toro_test_2_z),
-           problem_module.CProblem(eos, *problem_module.toro_test_3_z),
-           problem_module.CProblem(eos, *problem_module.toro_test_4_z),
-           problem_module.CProblem(eos, *problem_module.toro_test_5_z)]
-
-NX = cfg.const['NX']
-NY = cfg.const['NY']
-NZ = cfg.const['NZ']
-field = [field.CField(problem[0], eos, NX, NY, NZ),
-         field.CField(problem[1], eos, NX, NY, NZ),
-         field.CField(problem[2], eos, NX, NY, NZ),
-         field.CField(problem[3], eos, NX, NY, NZ),
-         field.CField(problem[4], eos, NX, NY, NZ)]
-output = [output_module.COutput(problem[0], eos, field[0]),
-          output_module.COutput(problem[1], eos, field[1]),
-          output_module.COutput(problem[2], eos, field[2]),
-          output_module.COutput(problem[3], eos, field[3]),
-          output_module.COutput(problem[4], eos, field[4])]
-solver = exc_module.CExactRiemannSolver(eos)
+#problem = [problem_module.CProblem(*problem_module.toro_test_1_z),
+#           problem_module.CProblem(*problem_module.toro_test_2_z),
+#           problem_module.CProblem(*problem_module.toro_test_3_z),
+#           problem_module.CProblem(*problem_module.toro_test_4_z),
+#           problem_module.CProblem(*problem_module.toro_test_5_z)]
+#NX = cfg.const['NX']
+#NY = cfg.const['NY']
+#NZ = cfg.const['NZ']
+#field = [field.CField(problem[0], eos, NX, NY, NZ),
+#         field.CField(problem[1], eos, NX, NY, NZ),
+#         field.CField(problem[2], eos, NX, NY, NZ),
+#         field.CField(problem[3], eos, NX, NY, NZ),
+#         field.CField(problem[4], eos, NX, NY, NZ)]
+#output = [output_module.COutput(problem[0], eos, field[0]),
+#          output_module.COutput(problem[1], eos, field[1]),
+#          output_module.COutput(problem[2], eos, field[2]),
+#          output_module.COutput(problem[3], eos, field[3]),
+#          output_module.COutput(problem[4], eos, field[4])]
+#solver = exc_module.CExactRiemannSolver(eos)
 # for i in range(5):
 #    app = app_module.CApp(problem[i], eos, field[i], solver, output[i])
 #    app.run()
 
-i = 4
-app = app_module.CApp(problem[i], eos, field[i], solver, output[i])
+problem_RTI = problem_module.CProblem()
+problem_RTI.init_RTI(*problem_module.yalinevich_test_y)
+NX = cfg.const['NX']
+NY = cfg.const['NY']
+NZ = cfg.const['NZ']
+solver = exc_module.CExactRiemannSolver(eos)
+field = field_module.CField(problem_RTI, eos, NX, NY, NZ)
+output = output_module.COutput(problem_RTI, eos, field)
+app = app_module.CApp(problem_RTI, eos, field, solver, output)
 app.run()
+
--- problem.py
+++ problem.py
@@ -1,12 +1,14 @@
 import config as cfg
 
 class CProblem:
-    def __init__(self, eos, name, dir, ro_l, u_l, p_l, ro_r, u_r, p_r, q_0, x_min, x_max, y_min, y_max, z_min, z_max, t_min, t_max, bcs):
+    def __init__(self, name=None, dir=None, ro_l=None, u_l=None, p_l=None, ro_r=None, u_r=None, p_r=None, q_0=None,
+                 x_min=None, x_max=None, y_min=None, y_max=None, z_min=None, z_max=None, t_min=None, t_max=None,
+                 bcs=None):
         """Конструктор одномерной задачи о распаде разрыва в 3D"""
         print("Class CProblem: ", end="")
-        print("Initializing one-dimensional desintegration of the discontinuity problem...", end="")
-        self.GAMMA = eos.GAMMA
+        print("Initializing...", end="")
         self.name = name
+        self.type = "RP"
         self.dir = dir
         self.ro_l = ro_l
         self.u_l = u_l		
@@ -15,6 +17,10 @@ class CProblem:
         self.u_r = u_r
         self.p_r = p_r
         self.q_0 = q_0
+        self.ro_up = -1.
+        self.ro_down = -1.
+        self.p_0 = -1.
+        self.g = -1.
         self.x_min = x_min
         self.x_max = x_max
         self.y_min = y_min
@@ -25,11 +31,34 @@ class CProblem:
         self.t_max = t_max 
         self.bcs = bcs
         self.CFL = cfg.const['CFL']
-        self.type = "RP"
+
         """Boundary conditions transcription: 'w' -- wall, 'p' -- periodic, 't' -- transmissive, 
         order (natural): left X-b.c., right X-b.c., left Y-b.c., right Y-b.c., left Z-b.c., right Z-b.c."""        
         print("done!")
-    
+
+    def init_RTI(self, name, dir, ro_down, ro_up, p_0, g, q_0,
+                 x_min, x_max, y_min, y_max, z_min, z_max, t_min, t_max, bcs):
+        """Конструктор задачи о неустойчивости Рэлея-Тейлора в 3D"""
+        self.name = name
+        self.type = "RTI"
+        self.dir = dir
+        self.ro_down = ro_down
+        self.ro_up = ro_up
+        self.p_0 = p_0
+        self.g = g
+        self.q_0 = q_0
+        self.x_min = x_min
+        self.x_max = x_max
+        self.y_min = y_min
+        self.y_max = y_max
+        self.z_min = z_min
+        self.z_max = z_max
+        self.t_min = t_min
+        self.t_max = t_max
+        self.bcs = bcs
+        self.CFL = cfg.const['CFL']
+        self.type = "RTI"
+
     def __str__(self):
         return "<%f %s %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %s>" % (self.GAMMA, self.name, self.dir,
                                                        self.ro_l, self.u_l, self.p_l, self.ro_r, self.u_r, self.p_r, self.q_0, 
@@ -37,6 +66,10 @@ class CProblem:
                                                        self.t_min, self.t_max, self.bcs)
 
 
+# Pre-installed test problems, suitable for code verification
+
+# 1D Toro tests, various Riemann problems
+# x-direction
 toro_test_1_x = ('toro-1-x', 'x',      1.,       .75,      1.,    .125,        0.,      .1, .3,
                  0., 1., 0., .1, 0., .1, 0., .2,    "tttttt")
 toro_test_2_x = ('toro-2-x', 'x',      1.,       -2.,      .4,      1.,        2.,      .4, .5,
@@ -47,7 +80,7 @@ toro_test_4_x = ('toro-4-x', 'x', 5.99924,   19.5975, 460.894, 5.99242,  -6.1963
                  0., 1., 0., .1, 0., .1, 0., .035,  "tttttt")
 toro_test_5_x = ('toro-5-x', 'x',      1., -19.59745,   1000.,      1., -19.59745,     .01, .8,
                  0., 1., 0., .1, 0., .1, 0., .012,  "tttttt")
-
+# y-direction
 toro_test_1_y = ('toro-1-y', 'y',      1.,       .75,      1.,    .125,        0.,      .1, .3,
                  0., .1, 0., 1., 0., .1, 0., .2,  "tttttt")
 toro_test_2_y = ('toro-2-y', 'y',      1.,       -2.,      .4,      1.,        2.,      .4, .5,
@@ -58,7 +91,7 @@ toro_test_4_y = ('toro-4-y', 'y', 5.99924,   19.5975, 460.894, 5.99242,  -6.1963
                  0., .1, 0., 1., 0., .1, 0., .035, "tttttt")
 toro_test_5_y = ('toro-5-y', 'y',      1., -19.59745,   1000.,      1., -19.59745,     .01, .8,
                  0., 1., 0., 1., 0., .1, 0., .012, "tttttt")
-
+# z-direction
 toro_test_1_z = ('toro-1-z', 'z',      1.,       .75,      1.,    .125,        0.,      .1, .3,
                  0., .1, 0., .1, 0., 1., 0., .2,  "tttttt")
 toro_test_2_z = ('toro-2-z', 'z',      1.,       -2.,      .4,      1.,        2.,      .4, .5,
@@ -69,3 +102,6 @@ toro_test_4_z = ('toro-4-z', 'z', 5.99924,   19.5975, 460.894, 5.99242,  -6.1963
                  0., .1, 0., .1, 0., 1., 0., .035, "tttttt")
 toro_test_5_z = ('toro-5-z', 'z',      1., -19.59745,   1000.,      1., -19.59745,     .01, .8,
                  0., 1., 0., .1, 0., 1., 0., .012, "tttttt")
+
+# 2D Rayleigh-Taylor instabilities
+yalinevich_test_y = ('yalinevich-RTI-y', 'y', 1., 2., 2.5, -.1, .75, 0., .5, 0., 1.5, 0., .1, 0., 15., 'wwwwww')
